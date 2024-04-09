f31bff29bb5e10f4b4ca8ba47d031555869b4d0b
jeffreylinus <60944161+jeffreylinus@users.noreply.github.com>
2024-04-09 18:09:28 -0400

    created database

---
+++ database.js
@@ -0,0 +1,11 @@
+const eventsDatabase = {
+    events: [
+      { name: "Earth Day Fair", onCampus: true , time: Date('2024-04-20T16:00:00Z') , free: true, category: "cultural" },
+      { name: "17th Annual Festival of New Italian Cinema - Film: \"Perfetti sconosciuti\" (Perfect Strangers)", onCampus: true , time: Date('2024-04-20T19:00:00Z') , free: true, category: "cultural" },
+      { name: "Chinese American Students Association Cultural Show", onCampus: true , time: Date('2024-04-21T22:00:00Z') , free: true, category: "performance" },
+      { name: "2024 AACC Awards Night!", onCampus: true , time: Date('2024-04-21T16:00:00Z') , free: true, category: "cultural" },
+      { name: "Morning Roast featuring the Holy Ghost by Andover Newton Seminary", onCampus: true , time: Date('2024-05-01T8:30:00Z') , free: true, category: "cultural" },
+      { name: "Sunday Workout", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, category: "sports" },
+      { name: "Nontraditional PhD Careers Panel", onCampus: true , time: Date('2024-05-01T18:00:00Z'), free: true, category: "speakers" },
+    ]
+};
