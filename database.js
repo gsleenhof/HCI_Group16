9ad0a1e644180873c51688842165f59c805fc74d
jeffreylinus <60944161+jeffreylinus@users.noreply.github.com>
2024-04-09 18:19:22 -0400

    updated database

--- database.js
+++ database.js
@@ -1,11 +1,14 @@
 const eventsDatabase = {
     events: [
-      { name: "Earth Day Fair", onCampus: true , time: Date('2024-04-20T16:00:00Z') , free: true, category: "cultural" },
-      { name: "17th Annual Festival of New Italian Cinema - Film: \"Perfetti sconosciuti\" (Perfect Strangers)", onCampus: true , time: Date('2024-04-20T19:00:00Z') , free: true, category: "cultural" },
-      { name: "Chinese American Students Association Cultural Show", onCampus: true , time: Date('2024-04-21T22:00:00Z') , free: true, category: "performance" },
-      { name: "2024 AACC Awards Night!", onCampus: true , time: Date('2024-04-21T16:00:00Z') , free: true, category: "cultural" },
-      { name: "Morning Roast featuring the Holy Ghost by Andover Newton Seminary", onCampus: true , time: Date('2024-05-01T8:30:00Z') , free: true, category: "cultural" },
-      { name: "Sunday Workout", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, category: "sports" },
-      { name: "Nontraditional PhD Careers Panel", onCampus: true , time: Date('2024-05-01T18:00:00Z'), free: true, category: "speakers" },
+      { name: "Earth Day Fair", onCampus: true , time: Date('2024-04-20T16:00:00Z') , free: true, weekend: true, category: "cultural" },
+      { name: "17th Annual Festival of New Italian Cinema - Film: \"Perfetti sconosciuti\" (Perfect Strangers)", onCampus: true, time: Date('2024-04-20T19:00:00Z') , free: true, weekend: true, category: "cultural" },
+      { name: "Chinese American Students Association Cultural Show", onCampus: true , time: Date('2024-04-21T22:00:00Z') , free: true, weekend: true, category: "performance" },
+      { name: "2024 AACC Awards Night!", onCampus: true , time: Date('2024-04-21T16:00:00Z') , free: true, weekend: true, category: "cultural" },
+      { name: "Morning Roast featuring the Holy Ghost by Andover Newton Seminary", onCampus: true , time: Date('2024-05-01T8:30:00Z') , free: true, weekend: true, category: "cultural" },
+      { name: "Sunday Workout", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, weekend: true, category: "sports" },
+      { name: "Gentle Yoga", onCampus: true , time: Date('2024-04-23T17:00:00Z'), free: true, weekend: false, category: "sports" },
+      { name: "Open Heart Night Prayer", onCampus: true , time: Date('2024-04-23T19:00:00Z'), free: true, weekend: false, category: "cultural" },
+      { name: "Basketball vs Brown University", onCampus: true , time: Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "sports" },
+      { name: "Nontraditional PhD Careers Panel", onCampus: true , time: Date('2024-05-01T18:00:00Z'), free: true, weekend: true, category: "speakers" },
     ]
 };
