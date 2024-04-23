0e599d43e8994187d996fb5bea4e1f11618650ec
jeffreylinus <60944161+jeffreylinus@users.noreply.github.com>
2024-04-23 13:11:58 -0400

    Update database.js

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const eventsDatabase = {
       { name: "South Asian Studies Council Chai", onCampus: true , time: Date('2024-05-25T4:30:00Z') , free: true, weekend: false, category: "Cultural" },
 
 
-            { name: "Basketball vs Brown University", onCampus: true , time: Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "Sports" },
+      { name: "Basketball vs Brown University", onCampus: true , time: Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "Sports" },
       { name: "Sunday Workout", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, weekend: true, category: "Sports" },
       
       { name: "Massaro Farm Volunteer Day", onCampus: false , time: Date('2024-04-25T12:00:00Z') , free: true, weekend: false, category: "Community Service" },
