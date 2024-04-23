de7c9de62f69cf22696e7fd4b2bc357ce67b5bfc
sahiti-r <132848338+sahiti-r@users.noreply.github.com>
2024-04-23 13:10:21 -0400

    updating database.js
    preliminary database post 4/25

--- database.js
+++ database.js
@@ -1,21 +1,19 @@
 const eventsDatabase = {
-  events: [
-      { name: "Earth Day Fair", onCampus: true, time: new Date('2024-04-27T16:00:00Z'), free: true, weekend: true, category: "cultural" },
-      { name: "Morning Roast featuring the Holy Ghost by Andover Newton Seminary", onCampus: false, time: new Date('2024-05-12T09:00:00Z'), free: true, weekend: true, category: "cultural" },
-      { name: "Nontraditional PhD Careers Panel", onCampus: false, time: new Date('2024-05-01T18:00:00Z'), free: true, weekend: false, category: "cultural" },
-      { name: "Cultural Crafts Workshop", onCampus: true, time: new Date('2024-04-29T14:00:00Z'), free: true, weekend: false, category: "cultural" },
+    events: [
+      { name: "Nontraditional PhD Careers Panel", onCampus: true , time: Date('2024-05-01T18:00:00Z'), free: true, weekend: true, category: "Cultural" },
+      { name: "Morning Roast featuring the Holy Ghost by Andover Newton Seminary", onCampus: true , time: Date('2024-05-01T8:30:00Z') , free: true, weekend: true, category: "Cultural" },
+      { name: "South Asian Studies Council Chai", onCampus: true , time: Date('2024-05-25T4:30:00Z') , free: true, weekend: false, category: "Cultural" },
 
-      { name: "17th Annual Festival of New Italian Cinema - Film: \"Perfetti sconosciuti\" (Perfect Strangers)", onCampus: true, time: new Date('2024-04-25T19:00:00Z'), free: true, weekend: false, category: "performance" },
-      { name: "Chinese American Students Association Cultural Show", onCampus: true, time: new Date('2024-04-28T22:00:00Z'), free: true, weekend: true, category: "performance" },
-      { name: "2024 AACC Awards Night!", onCampus: true, time: new Date('2024-04-28T16:00:00Z'), free: true, weekend: true, category: "performance" },
-      { name: "Open Heart Night Prayer", onCampus: false, time: new Date('2024-04-30T19:00:00Z'), free: true, weekend: false, category: "performance" },
-      { name: "Film Screening: Local Directors", onCampus: false, time: new Date('2024-04-28T20:00:00Z'), free: true, weekend: true, category: "performance" },
 
-      { name: "Sunday Workout", onCampus: true, time: new Date('2024-04-28T11:00:00Z'), free: true, weekend: true, category: "sports" },
-      { name: "Gentle Yoga", onCampus: true, time: new Date('2024-04-29T17:00:00Z'), free: true, weekend: false, category: "sports" },
-      { name: "Hot Yoga", onCampus: false, time: new Date('2024-04-29T17:00:00Z'), free: true, weekend: false, category: "sports" },
-      { name: "Basketball vs Brown University", onCampus: true, time: new Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "sports" },
-      { name: "Outdoor Yoga Session", onCampus: false, time: new Date('2024-05-05T09:00:00Z'), free: true, weekend: true, category: "sports" },
-      { name: "Indoor Track Meet", onCampus: true, time: new Date('2024-04-30T10:00:00Z'), free: true, weekend: false, category: "sports" }
-  ]
+            { name: "Basketball vs Brown University", onCampus: true , time: Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "Sports" },
+      { name: "Sunday Workout", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, weekend: true, category: "Sports" },
+      
+      { name: "Massaro Farm Volunteer Day", onCampus: false , time: Date('2024-04-25T12:00:00Z') , free: true, weekend: false, category: "Community Service" },
+      
+      { name: "AI in Medicine Monthly Seminar - Dr. Hua Xu", onCampus: true , time: Date('2024-04-25T01:00:00Z') , free: true, weekend: false, category: "Speaker Event" },
+
+      
+      { name: "", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, weekend: false, category: "Community Service" },
+
+    ]
 };
