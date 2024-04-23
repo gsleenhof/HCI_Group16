ccad9d63aa15dedff053b2b1f97fa6566a5f0d85
sahiti-r <132848338+sahiti-r@users.noreply.github.com>
2024-04-23 13:30:58 -0400

    Update database.js
    adding links

--- database.js
+++ database.js
@@ -3,17 +3,20 @@ const eventsDatabase = {
       { name: "Nontraditional PhD Careers Panel", onCampus: true , time: Date('2024-05-01T18:00:00Z'), free: true, weekend: true, category: "Cultural" },
       { name: "Morning Roast featuring the Holy Ghost by Andover Newton Seminary", onCampus: true , time: Date('2024-05-01T8:30:00Z') , free: true, weekend: true, category: "Cultural" },
       { name: "South Asian Studies Council Chai", onCampus: true , time: Date('2024-05-25T4:30:00Z') , free: true, weekend: false, category: "Cultural" },
+      
+      { name: "Massaro Farm Volunteer Day", onCampus: false , time: Date('2024-04-25T12:00:00Z') , free: true, weekend: false, category: "Community Service", link: "https://yaleconnect.yale.edu/SWAY/rsvp_boot?id=2270427"},
+      { name: "Volunteer with Sunrise Cafe - Service Series", onCampus: false, time: Date('2024-04-30T6:00:00Z') , free: true, weekend: false, category: "Community Service", link: "https://yaleconnect.yale.edu/DwightHall/rsvp_boot?id=2263505"},
 
 
       { name: "Basketball vs Brown University", onCampus: true , time: Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "Sports" },
-      { name: "Sunday Workout", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, weekend: true, category: "Sports" },
+      { name: "Trip to a New York City Football Club Tailgate & Game at Yankees Stadium!", onCampus: true , time: Date('2024-04-27T11:30:00Z'), free: false, weekend: true, category: "Sports", link: "https://www.eventbrite.com/e/trip-to-a-new-york-city-football-club-tailgate-game-at-yankees-stadium-tickets-876025454717?aff=oddtdtcreator",
       
-      { name: "Massaro Farm Volunteer Day", onCampus: false , time: Date('2024-04-25T12:00:00Z') , free: true, weekend: false, category: "Community Service" },
+      { name: "Rangeela Showcase 2024", onCampus: true , time: Date('2024-04-26T20:00:00Z') , free: true, weekend: false, category: "Performance", link: "https://yaleconnect.yale.edu/YCRangeela/rsvp_boot?id=2270082"},
+    
       
-      { name: "AI in Medicine Monthly Seminar - Dr. Hua Xu", onCampus: true , time: Date('2024-04-25T01:00:00Z') , free: true, weekend: false, category: "Speaker Event" },
-
       
-      { name: "", onCampus: true , time: Date('2024-04-21T11:00:00Z') , free: true, weekend: false, category: "Community Service" },
+      { name: "AI in Medicine Monthly Seminar - Dr. Hua Xu", onCampus: true , time: Date('2024-04-25T01:00:00Z') , free: true, weekend: false, category: "Speaker Event", link: "https://yaleconnect.yale.edu/AIM/rsvp_boot?id=2270961"},
+      { name: "Bridging GAPS Diversity in STEM Symposium", onCampus: true , time: Date('2024-04-26T9:30:00Z') , free: true, weekend: false, category: "Speaker Event", link: "https://yaleconnect.yale.edu/gaps/rsvp_boot?id=2268919"},
 
     ]
 };
